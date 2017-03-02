523ccd930f45ae306a8bed9bc370041acb616c07
Caelan Garrett <caelan@mit.edu>
2017-03-02 14:31:10 -0500

    Some stuff is working

--- scripts/detect_clusters_caelan.py
+++ scripts/detect_clusters_caelan.py
@@ -18,8 +18,29 @@ from std_msgs.msg import Header, ColorRGBA
 from threading import Thread
 from openravepy import quatFromRotationMatrix
 from scipy.spatial import ConvexHull
+from time import time
 
-def or_quat_to_ros_quat(or_quat):
+from openravepy import Environment
+from manipulation.bodies.bodies import box_body
+from manipulation.primitives.transforms import set_pose, set_point, get_point
+from manipulation.constants import GRASP_APPROACHES, GRASP_TYPES
+
+from lis_pr2_pkg.uber_controller import Uber
+from pick_and_place import feasible_pick_place, get_grasps, initialize_openrave
+
+def ros_point_to_or_point(ros_point):
+  return np.array([ros_point.x, ros_point.y, ros_point.z])
+
+def ros_quat_to_or_quat(ros_quat):
+  return np.array([ros_quat.w, ros_quat.x, ros_quat.y, ros_quat.z])
+
+def ros_pose_to_or_pose(ros_pose):
+  return np.concatenate([
+    ros_quat_to_or_quat(ros_pose.orientation),
+    ros_point_to_or_point(ros_pose.position), 
+  ])
+
+def or_quat_to_ros_quat(or_quat): # TODO - make this return a quat object
   return np.concatenate([or_quat[1:], or_quat[:1]])
 
 # def oobb_from_points_2D(points): # NOTE - not necessarily optimal
@@ -51,7 +72,7 @@ def oobb_from_points_2D(points): # NOTE - not necessarily optimal
 
   return mu, extents, or_quat_to_ros_quat(quatFromRotationMatrix(trans))
 
-
+# Do the 2D projection
 def oobb_from_points(points): # NOTE - not necessarily optimal
   mean = np.mean(points, axis=1)
 
@@ -69,11 +90,26 @@ def oobb_from_points(points): # NOTE - not necessarily optimal
 
   return mean, extents, or_quat_to_ros_quat(quatFromRotationMatrix(trans))
 
+def oobb_from_points2(points):
+    import trimesh
+    mesh = trimesh.convex.convex_hull(points)
+    print 'Vertices:', len(mesh.vertices)
+    oobb = mesh.bounding_box_oriented
+    trans = oobb.primitive.transform
+    return trans[:3, 3], oobb.primitive.extents, or_quat_to_ros_quat(quatFromRotationMatrix(trans[:3,:3]))
+
 def aabb_from_points(points): # NOTE - not necessarily optimal
   mu = np.mean(points, axis=1)
   extents = np.max(points, axis=1) - np.min(points, axis=1)
   trans = np.array([0, 0, 0, 1])
   return mu, extents, trans
+
+# env = Environment()
+# #robot = env.ReadRobotXMLFile('robots/pr2-beta-sim.robot.xml') # ReadRobotData
+# robot = env.ReadRobotXMLFile('robots/pr2-beta-static.zae')
+# env.Add(robot)
+# initialize_openrave(env)
+# quit()
 
 class ClusterFilter():
     table_frame = "head_mount_kinect_rgb_optical_frame"
@@ -93,6 +129,18 @@ class ClusterFilter():
 
 
     def __init__(self, tf_listener=None):
+        self.uc = Uber()
+        self.detected = False
+
+        self.env = Environment()
+        #self.robot = self.env.ReadRobotXMLFile('robots/pr2-beta-sim.robot.xml') # ReadRobotData
+        self.robot = self.env.ReadRobotXMLFile('robots/pr2-beta-static.zae')
+        self.env.Add(self.robot)
+        initialize_openrave(self.env)
+
+        self.robot.SetDOFValues(np.array([self.uc.get_torso_pose()]), [self.robot.GetJointIndex('torso_lift_joint')])
+        self.robot.SetDOFValues(self.uc.get_joint_positions('l'), self.robot.GetManipulator('leftarm').GetArmIndices())
+        self.robot.SetDOFValues(self.uc.get_joint_positions('r'), self.robot.GetManipulator('rightarm').GetArmIndices())
 
         if tf_listener == None:
             self.tf_listener = tf.TransformListener()
@@ -147,20 +195,10 @@ class ClusterFilter():
             ros_pts = [Point(*p) for p in points]
             points = np.array(points)
 
-            print points.shape
-            #(x, y, z), (w, h, l), quat = aabb_from_points(points.T)
-            #(x, y, z), (w, h, l), quat = oobb_from_points(points.T)
-            (x, y, z), (w, h, l), quat = oobb_from_points_2D(points.T)
-            print (x, y, z), (w, h, l)
-            print quat
-
-
             # TODO - the heights of the bounding boxes are off
-            #x,y,z = np.mean(points, axis=0)
-            #w,h,l = np.max(points,axis=0) - np.min(points,axis=0)
-            #quat = np.array([0, 0, 0, 1])
-            #print (x, y, z), (w, h, l)
-            #print
+            x,y,z = np.mean(points, axis=0)
+            w,h,l = np.max(points,axis=0) - np.min(points,axis=0)
+            quat = np.array([0, 0, 0, 1])
 
             area = w*h*l
             density = len(points)/area
@@ -173,6 +211,15 @@ class ClusterFilter():
             if z < table[2]: continue
             # box to high off table
             if z > (0.2 + table[2]): continue
+
+            t0 = time()
+            #(x, y, z), (w, h, l), quat = aabb_from_points(points.T)
+            #(x, y, z), (w, h, l), quat = oobb_from_points(points.T)
+            #(x, y, z), (w, h, l), quat = oobb_from_points_2D(points.T)
+            #(x, y, z), (w, h, l), quat = oobb_from_points2(points)
+            print points.shape, time() - t0
+            print (x, y, z), (w, h, l)
+            #print quat
 
 
             m= deepcopy(cluster)
@@ -212,11 +259,64 @@ class ClusterFilter():
             boxes.append(bb)
             boxes.append(bbtext)
 
-
-
+        #[ 1.          0.          0.          0.          0.74781157  0.18362756
+        #  0.77238349]
+        #[[ 0.0551712  -0.83510342  0.5473193   0.16578753]
+        # [-0.99847053 -0.04418373  0.03323258  0.00290718]
+        # [-0.00357003 -0.54831567 -0.83626382  1.68669464]
+        # [ 0.          0.          0.          1.        ]]
 
         self.pub_clusters.publish(MarkerArray(markers))
         self.pub_bb.publish(MarkerArray(boxes))
+
+        if not self.detected and len(results) != 0:
+            rospy.loginfo("detection")
+            self.detected = True
+            print '\n'*10
+            _, _, _, _, bb = results[0]
+
+            print get_point(self.robot)
+            set_point(self.robot, [0, 0, 0])
+
+            body = box_body(self.env, bb.scale.x, bb.scale.y, bb.scale.z, name='obj', color=(1, 0, 0))
+            pose = ros_pose_to_or_pose(bb.pose)
+            set_pose(body, pose)
+            print pose, get_point(body)
+
+            #print self.uc.
+            self.env.Add(body)
+            print self.T
+
+            approach_config = self.robot.GetDOFValues(self.robot.GetActiveManipulator().GetArmIndices())
+
+            grasp_approach = GRASP_APPROACHES.TOP # TOP | SIDE
+            grasp_type = GRASP_TYPES.TOUCH # TOUCH
+            do_motion_planning = False
+            viewer = False
+
+            self.env.Save('/home/caelan/catkin_wsd/src/lis_pr2_pkg_experimental/scripts/env.dae', Environment.SelectionOptions.Everything) 
+            rospy.loginfo("Saved!")
+
+            results = None
+            with body:
+                with self.robot:
+                    grasps = get_grasps(self.env, self.robot, body, grasp_approach, grasp_type)
+                    grasps = filter(lambda g: g.grasp_trans[0,3] > 0, grasps)
+                    results = feasible_pick_place(self.env, self.robot, body, grasps, approach_config, do_motion_planning)
+
+            if results is not None:
+                grasp_config, vec_traj, arm_traj = results
+                rospy.loginfo("Success pick")
+                print results
+                print 'Success!'
+                self.robot.SetDOFValues(grasp_config, self.robot.GetActiveManipulator().GetArmIndices())
+            else:
+                rospy.loginfo("Failed pick")
+                print 'Fail'
+
+            #self.sub.unregister()
+            if viewer:
+                self.env.SetViewer('qtcoin')
 
     def updateTransform(self):
         # continuously find transform for head to baselink
